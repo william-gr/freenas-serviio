c119a9ae28f48ec9a09b2b31303f320db5139134
William Grzybowski <william@agencialivre.com.br>
2012-07-12 09:53:03 -0300

    Initial commit of serviio plugin from FreeNAS
    This work has been done from protosd, its a mirror with some local
    changes to help development.
    
    (Removed serviio.db)

---
+++ nanobsd/plugins/serviio/resources/scripts_local/jdownloader.sh
@@ -0,0 +1,3 @@
+##/usr/local/bin/bash
+#export DISPLAY=remote-display-ip:0.0
+java -Xmx512m -jar /usr/pbi/serviio-`uname -m`/etc/local/JDownloader/JDownloader.jar &
---
+++ nanobsd/plugins/serviio/resources/serviioUI/freenas/utils.py
@@ -0,0 +1,68 @@
+from subprocess import Popen, PIPE
+import os
+import platform
+
+serviio_pbi_path = "/usr/pbi/serviio-" + platform.machine()
+serviio_etc_path = os.path.join(serviio_pbi_path, "etc")
+serviio_mnt_path = os.path.join(serviio_pbi_path, "mnt")
+serviio_fcgi_pidfile = "/var/run/serviio_fcgi_server.pid"
+serviio_fcgi_wwwdir = os.path.join(serviio_pbi_path, "www")
+serviio_control = "/usr/local/etc/rc.d/serviio"
+serviio_config = os.path.join(serviio_etc_path, "mt-daapd.conf")
+serviio_icon = os.path.join(serviio_pbi_path, "default.png")
+serviio_oauth_file = os.path.join(serviio_pbi_path, ".oauth")
+
+
+def get_rpc_url(request):
+    return 'http%s://%s/plugins/json-rpc/v1/' % ('s' if request.is_secure() \
+            else '', request.get_host(),)
+
+
+def get_serviio_oauth_creds():
+    f = open(serviio_oauth_file)
+    lines = f.readlines()
+    f.close()
+
+    key = secret = None
+    for l in lines:
+        l = l.strip()
+
+        if l.startswith("key"):
+            pair = l.split("=")
+            if len(pair) > 1:
+                key = pair[1].strip()
+
+        elif l.startswith("secret"):
+            pair = l.split("=")
+            if len(pair) > 1:
+                secret = pair[1].strip()
+
+    return key, secret
+
+
+serviio_advanced_vars = {
+    "set_cwd": {
+        "type": "checkbox",
+        "on": "-a",
+        },
+    "debuglevel": {
+        "type": "textbox",
+        "opt": "-d",
+        },
+    "debug_modules": {
+        "type": "textbox",
+        "opt": "-D",
+        },
+    "disable_mdns": {
+        "type": "checkbox",
+        "on": "-m",
+        },
+    "non_root_user": {
+        "type": "checkbox",
+        "on": "-y",
+        },
+    "ffid": {
+        "type": "textbox",
+        "opt": "-b",
+        },
+}
