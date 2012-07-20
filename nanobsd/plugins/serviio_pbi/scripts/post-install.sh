e49b2705f987a21bab3638077e7d4371e7f92a23
William Grzybowski <william@agencialivre.com.br>
2012-07-20 09:05:50 -0300

    Get rid of deprecated serviio and change post-install to use a shell var

--- nanobsd/plugins/serviio_pbi/scripts/post-install.sh
+++ nanobsd/plugins/serviio_pbi/scripts/post-install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+SERVIIO_HOME=/usr/pbi/serviio-`uname -m`
+
 # New strategy, put syncdb first to see if it works here
-/usr/pbi/serviio-`uname -m`/bin/python /usr/pbi/serviio-`uname -m`/serviioUI/manage.py syncdb --migrate --noinput
+${SERVIIO_HOME}/bin/python ${SERVIIO_HOME}/serviioUI/manage.py syncdb --migrate --noinput
 
 echo libz.so.4 libz.so.5 > /etc/libmap.conf
-echo libz.so.4 libz.so.5 > /usr/pbi/serviio-`uname -m`/etc/libmap.conf
+echo libz.so.4 libz.so.5 > ${SERVIIO_HOME}/etc/libmap.conf
 
 # check if this user/group already exists for the MiniDLNA Plugin
 
@@ -18,39 +20,39 @@ echo libz.so.4 libz.so.5 > /usr/pbi/serviio-`uname -m`/etc/libmap.conf
 
 #if [ `grep -c dlna /etc/passwd` -eq 1 ]
 #then
-    #touch /usr/pbi/serviio-`uname -m`/DLNA.exists
+    #touch ${SERVIIO_HOME}/DLNA.exists
 #fi
 
 #if [ `grep -c dlna /etc/passwd` -eq 0 ]
 #then
-#    pw useradd dlna -g dlna -G wheel -s /usr/local/bin/bash -d /usr/pbi/serviio-`uname -m`/MEDIA -w none
+#    pw useradd dlna -g dlna -G wheel -s /usr/local/bin/bash -d ${SERVIIO_HOME}/MEDIA -w none
 #fi
 
-mkdir -p /usr/pbi/serviio-`uname -m`/etc/serviio/home
+mkdir -p ${SERVIIO_HOME}/etc/serviio/home
 pw groupadd dlna
-pw useradd dlna -g dlna -G wheel -s /usr/local/bin/bash -d /usr/pbi/serviio-`uname -m`/etc/serviio/home -w none
+pw useradd dlna -g dlna -G wheel -s /usr/local/bin/bash -d ${SERVIIO_HOME}/etc/serviio/home -w none
 
-#chmod 775 /usr/pbi/serviio-`uname -m`/var/log
-#chmod 775 /usr/pbi/serviio-`uname -m`/var/db
+#chmod 775 ${SERVIIO_HOME}/var/log
+#chmod 775 ${SERVIIO_HOME}/var/db
 
-#chmod 775 /usr/pbi/serviio-`uname -m`/etc/mail
-#chmod 664 /usr/pbi/serviio-`uname -m`/etc/mail/aliases
+#chmod 775 ${SERVIIO_HOME}/etc/mail
+#chmod 664 ${SERVIIO_HOME}/etc/mail/aliases
 
-chown dlna:dlna /usr/pbi/serviio-`uname -m`/MEDIA
-chmod 775 /usr/pbi/serviio-`uname -m`/MEDIA
+chown dlna:dlna ${SERVIIO_HOME}/MEDIA
+chmod 775 ${SERVIIO_HOME}/MEDIA
 
 # Copy patched RC file over automatically generated one
-mkdir -p /usr/pbi/serviio-`uname -m`/etc/rc.d/
-chmod 755 /usr/pbi/serviio-`uname -m`/serviio.RC
-mv /usr/pbi/serviio-`uname -m`/serviio.RC /usr/pbi/serviio-`uname -m`/etc/rc.d/serviio
+mkdir -p ${SERVIIO_HOME}/etc/rc.d/
+chmod 755 ${SERVIIO_HOME}/serviio.RC
+mv ${SERVIIO_HOME}/serviio.RC ${SERVIIO_HOME}/etc/rc.d/serviio
 
 # The following 2 sed commands let Serviio determine the Jail IP address and add it to the JAVA_OPTS used to start Serviio
 
 sed -i '' -e "21a\\
-JAIL_IP=\`ifconfig  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}\'" /usr/pbi/serviio-`uname -m`/sbin/serviiod
+JAIL_IP=\`ifconfig  | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}\'" ${SERVIIO_HOME}/sbin/serviiod
 
 sed -i '' -e "22a\\
-JAVA_OPTS=\"\${JAVA_OPTS} -Dserviio.remoteHost=${JAIL_IP}\"" /usr/pbi/serviio-`uname -m`/sbin/serviiod
+JAVA_OPTS=\"\${JAVA_OPTS} -Dserviio.remoteHost=${JAIL_IP}\"" ${SERVIIO_HOME}/sbin/serviiod
 
 
 # Need to improve this in case hostname had another ip address or address had a different hostname
@@ -58,5 +60,5 @@ if [ `grep -c $JAIL_IP /etc/hosts` -eq 0 ]
     echo $JAIL_IP"	"`hostname` >> /etc/hosts
 fi
 
-echo 'serviio_flags=""' > /usr/pbi/serviio-`uname -m`/etc/rc.conf
+echo 'serviio_flags=""' > ${SERVIIO_HOME}/etc/rc.conf
 echo 'serviio_flags=""' > /etc/rc.conf
